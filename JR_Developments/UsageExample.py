a378ef4166ecd23a6c6fb71482daffa3be47b72f
Johan Reimann <reimann@ge.com>
2017-11-17 17:44:49 -0500

    Small change to the color calibration section in the example

--- JR_Developments/UsageExample.py
+++ JR_Developments/UsageExample.py
@@ -10,7 +10,7 @@ from GISDataFunctions import helper_functions as hf
 from GISDataFunctions import WVCalKernel as WVCK
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
-#import numpy as np
+
 
 # Open NTF file for processing
 img = 'D:/Core3D/Data/jacksonville/satellite_imagery/WV2/MSI/05SEP16WV021200016SEP05162552-M1BS-500881026010_01_P006_________GA_E0AAAAAAIAAG0.NTF'
@@ -31,7 +31,7 @@ m = Basemap(projection='cyl',llcrnrlon=bounds[3,1]-.1,llcrnrlat=bounds[3,0]-.1,u
 
 # Plot a map for the specified view: xpixel for a higher resolution can be set
 # to 2000 instead of 200
-#m.arcgisimage(service='ESRI_Imagery_World_2D', xpixels=200, verbose=True, zorder=1).get_array()
+m.arcgisimage(service='ESRI_Imagery_World_2D', xpixels=200, verbose=True, zorder=1).get_array()
 
 # Draw roads from osm_roads:
 m.readshapefile('D:/Core3D/development/ex_FgMKU8FtfzgKJNgmUTE7T3Y5E1cgb_osm_roads', 'Streets',drawbounds = False)
@@ -53,10 +53,10 @@ croppedData, x0, y0, x1, y1 = hf.getPatch(cornerPoints, data, rpcInformation)
 
 # Image Calibration:
 wv2 = WVCK.WV2params()
-test = WVCK.RadiometricCalibrator(img, wv2,sub_array = croppedData)
-
+calibratedData = WVCK.RadiometricCalibrator(img, wv2,sub_array = croppedData)
+calibratedData.calibrate()
 # get the associated lat long information for every point in the image:
-long, lat, plotPatch = hf.projectImage(croppedData, x0, y0, x1, y1, cornerPoints, rpcInformation)
+long, lat, plotPatch = hf.projectImage(calibratedData.get_calibrated_data(), x0, y0, x1, y1, cornerPoints, rpcInformation)
 
 # plot the result:
 m.pcolormesh(long,lat,plotPatch[:,:,1],cmap='gray')
